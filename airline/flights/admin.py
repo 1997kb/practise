7496f65cc42bc71197cade406c564a69075fd46f
1997kb <71140277+1997kb@users.noreply.github.com>
2021-04-07 17:21:16 +0545

    check it

---
+++ airline/flights/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Flight,Airport,Passenger
+
+admin.site.register(Airport)
+admin.site.register(Flight)
+admin.site.register(Passenger)
+
+
+
+
+# Register your models here.
